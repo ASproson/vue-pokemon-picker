a4ccd643e205a4131b1b4ed22406349ca60cb351
atlas <a.sproson@outlook.com>
2022-07-08 19:06:37 +0100

    Stores data in state

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div className="p-14">
-    <router-link class="flex justify-center text-4xl text-yellow-400" to="/">Pokemon Picker</router-link>
+    <router-link class="flex justify-center text-4xl text-yellow-400" to="/"
+      >Pokemon Picker</router-link
+    >
   </div>
   <router-view />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="home">
     <h3>Hello world</h3>
+    {{ pokemons }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { reactive, toRefs } from "vue";
 export default {
-  name: "HomeView",
-  setup(){
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {},
+    });
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
-      })
-  }
+        console.log(data);
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+          {}
+        );
+      });
+
+    return { ...toRefs(state) };
+  },
 };
 </script>
